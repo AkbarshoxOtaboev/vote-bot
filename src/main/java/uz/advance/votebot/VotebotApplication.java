5933469dae1c80377c70013eee1cbe5974a854c3
AkbarshoxOtaboev <otaboevakbar96@gmail.com>
2024-02-28 22:16:47 +0500

    empty project

---
+++ src/main/java/uz/advance/votebot/VotebotApplication.java
@@ -0,0 +1,13 @@
+package uz.advance.votebot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotebotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotebotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/advance/votebot/VotebotApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.advance.votebot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotebotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
